2bf206862d3b24f6bb84d658250723dee00a5a33
axie4 <91627871+axie4@users.noreply.github.com>
2022-02-03 12:29:21 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+function setBudget (num: number) {
+    Budget = num
+    if (Budget < 20000) {
+    	
+    } else if (Budget < 20000) {
+    	
+    }
+}
+function setFuel (fuel: string) {
+    fuelType = fuel
+    if (true) {
+    	
+    } else if (false) {
+    	
+    }
+}
+let fuelType = ""
+let Budget = 0
+game.showLongText("Welcome to the Car Finder Tool", DialogLayout.Top)
+setBudget(game.askForNumber("What is your budget?"))
