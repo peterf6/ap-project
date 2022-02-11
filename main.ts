14f953ae8b733680e91a389a9d154eeedabf6315
axie4 <91627871+axie4@users.noreply.github.com>
2022-02-11 13:33:53 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,6 +20,18 @@ function setCarUse60g (offroadYorN: string) {
         game.showLongText("Error: option unavailable", DialogLayout.Bottom)
     }
 }
+// This function contains the final question in our code to find the user their perfect car. This functions takes the data on wheter the user selcted to drive their car off road or not and presents them with the perfect car based on their response. 
+function setCarUse81g (offroadYorN: string) {
+    if (offroadYorN == "y") {
+        game.showLongText("Land Rover Defender", DialogLayout.Bottom)
+        findCarSuccessful += 1
+    } else if (offroadYorN == "n") {
+        game.showLongText("Audi RS6", DialogLayout.Bottom)
+        findCarSuccessful += 1
+    } else {
+        game.showLongText("Error: option unavailable", DialogLayout.Bottom)
+    }
+}
 function setFuel80 (fuel: string) {
     if (fuel == "e") {
         setCarUse80e(game.askForString("Will this car be used offroad? (y or n)", 1))
@@ -29,6 +41,7 @@ function setFuel80 (fuel: string) {
         setCarUse80g(game.askForString("Will this car be used offroad? (y or n)", 1))
     }
 }
+// This function sets up what budget will be used and after a budget is set the function then sends the user to ask if they want an electric gas or hybrid car. 
 function setBudget (Budget: number) {
     if (Budget < 15000) {
         game.showLongText("Get yo money up not yo funny up", DialogLayout.Bottom)
@@ -62,8 +75,15 @@ function setFuel40 (fuel: string) {
         setCarUse40g(game.askForString("Will this car be used offroad? (y or n)", 1))
     }
 }
+// This function is for cars over 80k. Users will be asked what type of fuel they want. Once selected they will be directed to ask if they want to use the car off road. 
 function setFuel81 (fuel: string) {
-	
+    if (fuel == "e") {
+        setCarUse80e(game.askForString("Will this car be used offroad? (y or n)", 1))
+    } else if (fuel == "h") {
+        setCarUse80h(game.askForString("Will this car be used offroad? (y or n)", 1))
+    } else if (fuel == "g") {
+        setCarUse80g(game.askForString("Will this car be used offroad? (y or n)", 1))
+    }
 }
 function setCarUse80h (offroadYorN: string) {
     if (offroadYorN == "y") {
@@ -82,6 +102,28 @@ function setCarUse40g (offroadYorN: string) {
         findCarSuccessful += 1
     } else if (offroadYorN == "n") {
         game.showLongText("Chrysler Pacifica", DialogLayout.Bottom)
+        findCarSuccessful += 1
+    } else {
+        game.showLongText("Error: option unavailable", DialogLayout.Bottom)
+    }
+}
+function setCarUse81h (offroadYorN: string) {
+    if (offroadYorN == "y") {
+        game.showLongText("Range Rover Hybrid", DialogLayout.Bottom)
+        findCarSuccessful += 1
+    } else if (offroadYorN == "n") {
+        game.showLongText("Porsche Cayenne E-Hybrid", DialogLayout.Bottom)
+        findCarSuccessful += 1
+    } else {
+        game.showLongText("Error: option unavailable", DialogLayout.Bottom)
+    }
+}
+function setCarUse81e (offroadYorN: string) {
+    if (offroadYorN == "y") {
+        game.showLongText("Tesla Model X", DialogLayout.Bottom)
+        findCarSuccessful += 1
+    } else if (offroadYorN == "n") {
+        game.showLongText("Porsche Cayenne E-Hybrid", DialogLayout.Bottom)
         findCarSuccessful += 1
     } else {
         game.showLongText("Error: option unavailable", DialogLayout.Bottom)
